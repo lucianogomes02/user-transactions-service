9dd8532e88e0c3aedb1238bce15201faa5c14198
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-27 18:56:12 -0300

    feat: implements user controller exception handler

---
+++ users-service/src/main/java/com/users/application/exceptions/ExceptionDto.java
@@ -0,0 +1,4 @@
+package com.users.application.exceptions;
+
+public record ExceptionDto(String message, String statusCode) {
+}
---
+++ users-service/src/main/java/com/users/application/exceptions/UserConstrollerExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.users.application.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserConstrollerExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFoundException(EntityNotFoundException e) {
+        ExceptionDto exceptionDto = new ExceptionDto("Usuário não encontrado", "404");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var erros = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(ValidationErrorData::new).toList());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        ExceptionDto exceptionDto = new ExceptionDto("Usuário inválido ou já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDto);
+    }
+
+    private record ValidationErrorData(String campo, String mensagem) {
+        public ValidationErrorData(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
