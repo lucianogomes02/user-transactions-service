730befd84b97054c49a2fcb06ad7164b4f40c794
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-27 04:14:37 -0300

    fix: remove rest template dependency injection

--- transactions-service/src/main/java/com/transactions/producers/TransactionProducer.java
+++ transactions-service/src/main/java/com/transactions/producers/TransactionProducer.java
@@ -5,15 +5,12 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class TransactionProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @Value(value = "${broker.queue.transaction.to.user}")
     private String routingKey;
