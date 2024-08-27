440920f5c7064e7051c6c19106ac1faeb97ae53a
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-27 04:04:09 -0300

    feat: implements transaction services

--- transactions-service/src/main/java/com/transactions/services/TransactionService.java
+++ transactions-service/src/main/java/com/transactions/services/TransactionService.java
@@ -1,9 +1,11 @@
 package com.transactions.services;
 
+import com.transactions.domain.aggregate.Transaction;
 import com.transactions.domain.value_objects.TransactionPublicDto;
 import com.transactions.domain.value_objects.TransactionRecordDto;
 import com.transactions.repositories.TransactionRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,16 @@ public class TransactionService {
 
     @Transactional
     public TransactionPublicDto createTransaction(TransactionRecordDto transactionRecordDto) {
-        return null;
+        var transaction = new Transaction();
+        BeanUtils.copyProperties(transactionRecordDto, transaction);
+        transaction = transactionRepository.save(transaction);
+        return new TransactionPublicDto(
+            transaction.getId().toString(),
+            transaction.getSenderId().toString(),
+            transaction.getReceiverId().toString(),
+            transaction.getAmount().toString(),
+            transaction.getCreatedAt().toString()
+        );
     }
 
     public List<TransactionPublicDto> getTransactions() {
