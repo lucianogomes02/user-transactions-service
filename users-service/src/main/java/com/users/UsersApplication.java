5bbf2264c36f01240015a4f509aa9b2a6a3fd6a3
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-27 15:34:35 -0300

    feat: implements API docs with SpringDoc for User Service

--- users-service/src/main/java/com/users/UsersApplication.java
+++ users-service/src/main/java/com/users/UsersApplication.java
@@ -1,8 +1,15 @@
 package com.users;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "Users API",
+		version = "1.0",
+		description = "API for managing users. Receives queued messages from the Transactions Service."
+))
 @SpringBootApplication
 public class UsersApplication {
 
--- users-service/src/main/java/com/users/controllers/UserController.java
+++ users-service/src/main/java/com/users/controllers/UserController.java
@@ -4,24 +4,29 @@ import com.users.domain.value_objects.UserPublicDto;
 import com.users.domain.value_objects.UserRecordDto;
 import com.users.domain.value_objects.UserTransactionDto;
 import com.users.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User Controller", description = "Endpoints for users")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create a new user")
     @PostMapping("/users")
     public ResponseEntity<UserPublicDto> createUser(@RequestBody @Valid UserRecordDto userRecordDto) {
         UserPublicDto user = userService.createUser(userRecordDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
+    @Operation(summary = "Get all users")
     @GetMapping("/users")
     public ResponseEntity<Iterable<UserPublicDto>> getAllUsers() {
         Iterable<UserPublicDto> users = userService.getAllUsers();
