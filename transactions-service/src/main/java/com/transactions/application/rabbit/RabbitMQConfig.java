1fbea48cd18f83575a293b284384c420860a1249
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-29 05:15:35 -0300

    feat: rollback on circuit breaker and queues definition

--- transactions-service/src/main/java/com/transactions/application/rabbit/RabbitMQConfig.java
+++ transactions-service/src/main/java/com/transactions/application/rabbit/RabbitMQConfig.java
@@ -1,23 +1,11 @@
 package com.transactions.application.rabbit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.transactions.application.exceptions.TransactionQueueErrorHandler;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.config.DirectRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
-import org.springframework.retry.RecoveryCallback;
-import org.springframework.retry.RetryContext;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class RabbitMQConfig {
@@ -26,84 +14,7 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue queue() {
-        return QueueBuilder.durable(queue)
-                .withArgument("x-dead-letter-exchange", "dead-letter-exchange")
-                .withArgument("x-dead-letter-routing-key", "dead-letter-routing-key")
-                .withArgument("x-message-ttl", 5000)
-                .build();
-    }
-
-    @Bean
-    public Queue deadLetterQueue() {
-        return QueueBuilder.durable("dead-letter-queue").build();
-    }
-
-    @Bean
-    public DirectExchange deadLetterExchange() {
-        return new DirectExchange("dead-letter-exchange");
-    }
-
-    @Bean
-    public Binding deadLetterBinding() {
-        return BindingBuilder.bind(deadLetterQueue())
-                .to(deadLetterExchange())
-                .with("dead-letter-routing-key");
-    }
-
-    @Bean
-    public RabbitListenerContainerFactory<DirectMessageListenerContainer> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        DirectRabbitListenerContainerFactory factory = new DirectRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-        factory.setMessageConverter(messageConverter());
-        factory.setConsumersPerQueue(3);
-        factory.setPrefetchCount(3);
-        factory.setErrorHandler(new TransactionQueueErrorHandler());
-        return factory;
-    }
-
-    @Bean
-    public RecoveryCallback<Void> republishMessageRecovereyCallback(RabbitTemplate template) {
-        RepublishMessageRecoverer recoverer = new RepublishMessageRecoverer(
-                template,
-                "dead-letter-exchange",
-                "dead-letter-routing-key"
-        );
-
-        return new RecoveryCallback<Void>() {
-            @Override
-            public Void recover(RetryContext context) {
-                Message failedMessage = (Message) context.getAttribute(RetryContext.EXHAUSTED);
-                recoverer.recover(failedMessage, context.getLastThrowable());
-                return null;
-            }
-        };
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        template.setRetryTemplate(retryTemplate());
-        template.setRecoveryCallback(republishMessageRecovereyCallback(template));
-        return template;
-    }
-
-    @Bean
-    public RetryTemplate retryTemplate() {
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-        retryPolicy.setMaxAttempts(3);
-        retryTemplate.setRetryPolicy(retryPolicy);
-
-        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
-        backOffPolicy.setInitialInterval(500);
-        backOffPolicy.setMultiplier(2.0);
-        backOffPolicy.setMaxInterval(5000);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        return retryTemplate;
+        return new Queue(queue, true);
     }
 
     @Bean
