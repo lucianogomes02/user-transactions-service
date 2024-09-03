565df9c5e8b1f5771822b7958487de87b965e560
Luciano Gomes <lucianogvda02@gmail.com>
2024-09-02 23:59:00 -0300

    fix: wallet migration

--- transactions-service/src/main/resources/db/migration/V4__create-wallet-update-transactions.sql
+++ transactions-service/src/main/resources/db/migration/V4__create-wallet-update-transactions.sql
@@ -3,9 +3,9 @@ CREATE TABLE tb_wallet (
     user_id UUID NOT NULL UNIQUE,
     balance DECIMAL(19, 2) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL,
 
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE tb_transactions CREATE COLUMN updated_at TIMESTAMP NOT NULL;
