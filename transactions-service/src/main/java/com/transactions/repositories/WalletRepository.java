f831908dda99779208d5206f280101b0f29e75f8
Luciano Gomes <lucianogvda02@gmail.com>
2024-09-02 21:31:55 -0300

    feat: implements wallet repository

---
+++ transactions-service/src/main/java/com/transactions/repositories/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.transactions.repositories;
+
+import com.transactions.domain.aggregate.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+}
