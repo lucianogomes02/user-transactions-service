29f2564b91c635d1784539d9059a3f212257abcf
Luciano Gomes <lucianogvda02@gmail.com>
2024-09-03 04:48:13 -0300

    feat: convert users table to mongo document

--- users-service/src/main/java/com/users/UsersApplication.java
+++ users-service/src/main/java/com/users/UsersApplication.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @OpenAPIDefinition(info = @Info(
 		title = "Users API",
 		version = "1.0",
 		description = "API for managing users. Receives queued messages from the Transactions Service."
 ))
+@EnableMongoRepositories
 @SpringBootApplication
 public class UsersApplication {
 
--- users-service/src/main/java/com/users/domain/aggregate/User.java
+++ users-service/src/main/java/com/users/domain/aggregate/User.java
@@ -2,40 +2,23 @@ package com.users.domain.aggregate;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
-@Entity(name = "User")
-@Table(name = "TB_USERS")
+@Document(collection = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    @Column(name = "name")
+    private String id;
     private String name;
-
-    @Column(name = "email", unique = true)
     private String email;
-
-    @Column(name = "password")
     private String password;
-
-    @Column(name = "cpf", unique = true)
     private String cpf;
-
-    @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
-
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt = LocalDateTime.now();
-
-    @Column(name = "is_active")
     private boolean isActive = true;
 }
